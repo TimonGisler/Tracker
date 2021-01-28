147e68e8cd4ff6648d5df0af2e587165f0143cc7
Timon.Gisler4 <timongisler@outlook.com>
2021-01-28 21:43:18 +0100

    Project create

---
+++ src/main/java/ch/bbbaden/tracker/OneInstance.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.tracker;
+
+/**
+ *
+ * @author timon
+ */
+public class OneInstance {
+    double Price;
+    long time;
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double Price) {
+        this.Price = Price;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+    
+    
+    
+}
